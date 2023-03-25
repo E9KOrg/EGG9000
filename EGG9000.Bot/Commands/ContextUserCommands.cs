5f31c2446302b5be3a4b5c493cb84e1d75371457
DavidArthurCole <40234707+DavidArthurCole@users.noreply.github.com>
2023-03-25 11:11:04 -0400

    Add command for viewing a user's inventory(ies)

--- EGG9000.Bot/Commands/ContextUserCommands.cs
+++ EGG9000.Bot/Commands/ContextUserCommands.cs
@@ -16,6 +16,7 @@ using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
@@ -28,6 +29,25 @@ namespace EGG9000.Bot.Commands
         [UserCommand(Name = "View User on EGG9000.com", AdminOnly = true)]
         public static async Task WebsiteLink(SocketUserCommand command) {
             await command.RespondAsync($"<https://egg9000.com/MyFarms/ViewUser?discordId={command.Data.Member.Id}>", ephemeral: true);
+        }
+
+        [UserCommand(Name = "View User's Inventory on WASMEGG", AdminOnly = true)]
+        public static async Task WasmeggLink(SocketUserCommand command, ApplicationDbContext db) {
+
+            var user = await db.DBUsers.FirstOrDefaultAsync(x => x.DiscordId == command.User.Id);
+            if (user == null){
+                await command.RespondAsync("⚠️ERROR: Unable to find backups for this user");
+                return;
+            } else {
+
+                StringBuilder sb = new StringBuilder();
+                foreach(var id in user.EggIncIds) {
+                    var backup = user.Backups.FirstOrDefault(x => x.EggIncId == id.Id);
+                    sb.Append(backup.UserName + ": " + $"<https://wasmegg.netlify.app/inventory-visualizer/?playerId={id}>\n\n");
+                }
+
+                await command.RespondAsync(sb.ToString(), ephemeral: true);
+            }
         }
     }
 }
