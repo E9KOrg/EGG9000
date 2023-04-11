f26075d58f9549da5ed49c1fe8090d46f9a8f29a
Joshua Johnson <kendrome@gmail.com>
2023-04-11 17:33:54 -0500

    Fixes to Break buttons

--- BotTesting/Commands/ContractSettings.cs
+++ BotTesting/Commands/ContractSettings.cs
@@ -29,7 +29,7 @@ namespace EGG9000.Bot.Commands {
                 await command.RespondAsync("ERROR: Unable to find user");
             } else {
                 var props = GetMyContractSettingsMessage(dbuser);
-                await command.RespondAsync(props.Content.Value, components: props.Components.Value, ephemeral: true);
+                await command.RespondAsync(props.Content.Value, components: props.Components.Value);//, ephemeral: true);
             }
         }
 
@@ -65,16 +65,19 @@ BG1 <t:1681138800:t>, BG2 <t:1681167600:t>, BG3 <t:1681196400:t>. BG1 is when ne
                     builder.WithButton("Filter Contract By Reward", "EnableFilter");
                 }
                 //Only show "take a break" if the user isn't already on one
+                builder.WithButton("Disable Auto Assigned Co-ops", "DisableAutoAssign");
+
                 if(reg.OnBreakUntil == default) {
                     builder.WithButton("Take a Break From Co-ops", "TakeABreak");
                 } else {
                     //Else show some options (add 1 day, add 1 week, stop early)
-                    props.Content += $"\nOn Break until <t:{reg.OnBreakUntil.ToUnixTimeSeconds}:R>\n";
-                    builder.WithButton("Add 1 Day to Break", "BreakAddDay");
-                    builder.WithButton("Add 1 Week to Break", "BreakAddWeek");
-                    builder.WithButton("Stop Break Early", "StopBreakEarly");
+                    props.Content += $"\nBreak ends <t:{reg.OnBreakUntil.ToUnixTimeSeconds()}:R> on <t:{reg.OnBreakUntil.ToUnixTimeSeconds()}:D>\n";
+                    var row = new ActionRowBuilder()
+                    .WithButton("Add 1 Day to Break", "BreakAddDay")
+                    .WithButton("Add 1 Week to Break", "BreakAddWeek")
+                    .WithButton("Stop Break Early", "StopBreakEarly");
+                    builder.AddRow(row);
                 }
-                builder.WithButton("Disable Auto Assigned Co-ops", "DisableAutoAssign");
                 props.Components = builder.Build();
             } else {
                 props.Content = $@"**My Contract Settings**
@@ -123,7 +126,7 @@ You are not configured to automatically be assigned co-ops.
             var dbuser = await db.DBUsers.FirstAsync(x => x.DiscordId == component.User.Id);
             var reg = dbuser.ContractRegistration;
             //Add 1 day to the DTO
-            reg.OnBreakUntil.AddDays(1);
+            reg.OnBreakUntil = reg.OnBreakUntil.AddDays(1);
             dbuser.ContractRegistration = reg;
             await db.SaveChangesAsync();
             var props = GetMyContractSettingsMessage(dbuser);
@@ -136,7 +139,7 @@ You are not configured to automatically be assigned co-ops.
             var dbuser = await db.DBUsers.FirstAsync(x => x.DiscordId == component.User.Id);
             var reg = dbuser.ContractRegistration;
             //Add 7 days to the DTO
-            reg.OnBreakUntil.AddDays(7);
+            reg.OnBreakUntil = reg.OnBreakUntil.AddDays(7);
             dbuser.ContractRegistration = reg;
             await db.SaveChangesAsync();
             var props = GetMyContractSettingsMessage(dbuser);
