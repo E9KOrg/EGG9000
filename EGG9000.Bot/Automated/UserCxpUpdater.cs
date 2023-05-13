5a80bd3d25912d9dbb7372f5e82cf85426b8d900
DavidArthurCole <40234707+DavidArthurCole@users.noreply.github.com>
2023-05-12 21:43:34 -0400

    Started implementation of 'redo leggacy threshold'

---
+++ EGG9000.Bot/Automated/UserCxpUpdater.cs
@@ -0,0 +1,76 @@
+﻿using EGG9000.Bot.EggIncAPI;
+using EGG9000.Common.Database;
+using Ei;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace EGG9000.Bot.Automated
+{
+    public class UserCxpUpdater : _UpdaterBase<UserCxpUpdater>
+    {
+        #region DelayCalculations
+        /* 
+         * Calculations for next runtime - will run at 11 AM EST on M,W,F
+        */
+        private static readonly int _dayOfWeek = (int)DateTime.Now.DayOfWeek;
+        private static readonly int _currentMinute = (60*DateTime.Now.Hour) + DateTime.Now.Minute;
+
+        //From DayOfWeek doc;
+        // Returns the day-of-week part of this DateTime. The returned value
+        // is an integer between 0 and 6, where 0 indicates Sunday, 1 indicates
+        // Monday, 2 indicates Tuesday, 3 indicates Wednesday, 4 indicates
+        // Thursday, 5 indicates Friday, and 6 indicates Saturday.
+        //
+        private static readonly int _nextRunDayDelay = _dayOfWeek switch
+        {
+            0 => 1, 1 => 0, 2 => 1, 3 => 0, 4 => 1, 5 => 0, 6 => 0, _ => 0
+        };
+
+        // 600 minutes past midnight of system time is 11 AM EST
+        private static readonly int _nextRunSecondDelay = 600 - _currentMinute;
+
+        // Determine total time the system should wait before refreshing next
+        private static readonly int _nextRunTimeDelay = (60 * 60 * 24 * _nextRunDayDelay) + _nextRunSecondDelay;
+        #endregion
+
+        public UserCxpUpdater(
+            IServiceProvider provider
+        ) : base(TimeSpan.FromDays(1), TimeSpan.FromSeconds(_nextRunTimeDelay), provider){}
+
+        public override async Task Run(object state, CancellationToken cancellationToken) {
+            var _db = _provider.CreateScope().ServiceProvider.GetRequiredService<ApplicationDbContext>();
+
+            //Get a list of all users that have at least one backup, and at least one custombackup in the DB
+            var users = await _db.DBUsers.AsQueryable().Where(x => x.Backups.Any() && x._CustomBackups.Any()).ToListAsync();
+
+            //Loop through each user in the DB
+            foreach(var user in users) {
+                
+                //Loop through each account of the user
+                foreach(var account in user.EggIncAccounts) {
+
+                    //Get every score of the user's contracts
+                    var scores = await ContractsAPI.Post<MyContracts, BasicRequestInfo>(new BasicRequestInfo(), account.Id);
+                    //Get the backup for the user's account
+                    var backup = user.Backups.AsQueryable().Where(x => x.EggIncId == account.Id).FirstOrDefault();
+
+                    if (backup != null) {
+                        foreach(var archivedFarm in backup.ArchivedFarms) {
+                            var score = scores.Contracts.FirstOrDefault(x => x.Contract.Identifier == archivedFarm.ContractId)?.Evaluation;
+                            if(score != null) {
+                                archivedFarm.EvaluationCxp = score.Cxp;
+                            } else archivedFarm.EvaluationCxp = 0.0;
+                        }
+                    }
+                }
+            }
+            await _db.SaveChangesAsync();
+        }
+    }
+}
