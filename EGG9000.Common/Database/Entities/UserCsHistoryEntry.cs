08c7d2871318aeff5d4bad9a02a14f2d604d53c8
DavidArthurCole <40234707+DavidArthurCole@users.noreply.github.com>
2023-05-13 13:15:53 -0400

    Rename class to be less conflicting

--- EGG9000.Common/Database/Entities/UserCsHistoryEntry.cs
+++ EGG9000.Common/Database/Entities/UserCsHistoryEntry.cs
@@ -8,12 +8,12 @@ using System.Threading.Tasks;
 
 namespace EGG9000.Common.Database.Entities
 {
-    public class UserCsHistory {
+    public class UserCsHistoryEntry {
         public string ContractIdentifier { get; set; }
         public string CoopIdentifier { get; set; }
         public double Cxp { get; set; }
 
-        public UserCsHistory(string contractIdentifier, string coopIdentifier, double cxp) {
+        public UserCsHistoryEntry(string contractIdentifier, string coopIdentifier, double cxp) {
             ContractIdentifier = contractIdentifier;
             CoopIdentifier = coopIdentifier;
             Cxp = cxp;
@@ -22,7 +22,7 @@ namespace EGG9000.Common.Database.Entities
         public override bool Equals(object obj) {
             if(obj is Ei.Contract contract) {
                 return contract.Identifier == ContractIdentifier;
-            } else if(obj is UserCsHistory history) {
+            } else if(obj is UserCsHistoryEntry history) {
                 return history.ContractIdentifier == ContractIdentifier;
             } else return false;
         }
