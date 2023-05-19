4fbd57d7ed78e2f17a5846c18fb1024ea94d0f70
Joshua Johnson <kendrome@gmail.com>
2023-05-19 11:05:15 -0500

    Small fixes

--- EGG9000.Bot/Program.cs
+++ EGG9000.Bot/Program.cs
@@ -72,11 +72,11 @@ await Host.CreateDefaultBuilder(args)
             //services.AddHostedService<CoopDeleteChannel>();
 
 
-            services.Configure<UpdaterOptions<CoopStatusUpdater>>(x => x.DelayStart = TimeSpan.FromMinutes(2));
+            //services.Configure<UpdaterOptions<CoopStatusUpdater>>(x => x.DelayStart = TimeSpan.FromHours(1));
             services.AddSingleton<CoopStatusUpdater>();
             services.AddHostedService<CoopStatusUpdater>(provider => provider.GetService<CoopStatusUpdater>());
 
-            services.Configure<UpdaterOptions<ContractUpdater>>(x => x.DelayStart = TimeSpan.FromMinutes(1));
+            services.Configure<UpdaterOptions<ContractUpdater>>(x => x.DelayStart = TimeSpan.FromHours(1));
             services.AddSingleton<ContractUpdater>();
             services.AddHostedService<ContractUpdater>(provider => provider.GetService<ContractUpdater>());
 
