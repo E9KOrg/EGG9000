68125eefa158fcdd550f13f063d979155fa1e866
DavidArthurCole <davidarthurcole23@gmail.com>
2023-07-19 23:08:22 -0400

    Add multi-user selection modal to prevent overload of stringbuilder

--- EGG9000.Bot/Commands/MiscCommandsSlash.cs
+++ EGG9000.Bot/Commands/MiscCommandsSlash.cs
@@ -27,6 +27,8 @@ using EGG9000.Common.JsonData.EiAfxData;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Primitives;
 using System.Globalization;
+using Google.Protobuf.WellKnownTypes;
+using static Ei.Backup.Types;
 
 namespace EGG9000.Bot.Commands {
     public static class MiscCommandsSlash {
@@ -47,60 +49,77 @@ namespace EGG9000.Bot.Commands {
                 return;
             }
 
-            var stringBuilder = new StringBuilder();
-            for(var i = 0; i < user.EggIncAccounts.Count; i++) {
-                var id = user.EggIncAccounts[i];
-                var backup = id.Backup;
-                if(backup == null) {
-                    continue;
+            var contentString = "";
+
+            if(user.EggIncAccounts.Count == 1) {
+                contentString = await CraftStringBuilder(user.EggIncAccounts.First(), quantity, quality, requestedArtifact);
+                await command.ModifyOriginalResponseAsync(x => {
+                    x.Embed = null;
+                    x.Content = $"\n{contentString}\n";
+                });
+            } else {
+                var builder = new ComponentBuilder();
+                foreach(var account in user.EggIncAccounts) {
+                    builder.WithButton($"{account.Name} {account.Backup?.EarningsBonus.ToEggString()}", customId: $"CraftAccountButton:{account.Id}|{((int)quality)}|{quantity}|{artifact}");
                 }
-
-                backup = new CustomBackup((await ContractsAPI.FirstContact(id.Id)).Backup);
-                if(i == 0) {
-                    stringBuilder.Append($"For **{backup.UserName}** to craft {quantity} T{(int)quality} {requestedArtifact.id}:");
-                    stringBuilder.AppendLine();
-                } else {
-                    stringBuilder.AppendLine();
-                    stringBuilder.Append("――――――――――――――――――――――――――――――――――――――――――――");
-                    stringBuilder.AppendLine();
-                    stringBuilder.Append($"For **{backup.UserName}** to craft {quantity} T{(int)quality} {requestedArtifact.id}:");
-                    stringBuilder.AppendLine();
-                }
-
-                var crafter = new Crafter(backup.ArtifactHall);
-                var basket = crafter.GetCraft(quantity, (int)quality, requestedArtifact.id);
-
-                stringBuilder.AppendFormat($"```{"Name",-20}{"Using",-8}{"Need",-8}{"Cost",-8}");
-                stringBuilder.AppendLine();
-                stringBuilder.Append("―――――――――――――――――――――――――――――――――――――――――――");
-                stringBuilder.AppendLine();
-
-                var ingredients = from kvp in basket.GetIngredients()
-                    orderby EggIncArtifacts.GetFamilyShorthand(kvp.Value.Tier.family) ascending, kvp.Value.Tier.tier_number descending
-                    select kvp;
-                foreach(var ingredient in ingredients) {
-                    stringBuilder.AppendFormat($"{$"T{ingredient.Value.Tier.tier_number} {EggIncArtifacts.GetFamilyShorthand(ingredient.Value.Tier.family)}",-20}");
-                    stringBuilder.AppendFormat($"{ingredient.Value.Use.Format(),-8}");
-                    stringBuilder.AppendFormat($"{ingredient.Value.GetNeed(),-8}");
-                    stringBuilder.AppendFormat($"{ingredient.Value.Cost.Format(),-8}");
-                    stringBuilder.AppendLine();
-                }
-
-                stringBuilder.AppendLine("```");
-                stringBuilder.Append($"Total Cost: **{basket.GetTotalCost().ToString("#,0", new CultureInfo("en-US"))} GE**");
-                stringBuilder.AppendLine();
-                var goldenEggs = backup.GoldenEggsEarned - backup.GoldenEggsSpent;
-                stringBuilder.Append(goldenEggs >= basket.GetTotalCost() ? "You have enough GE!" : "You do not have enough GE!");
+                await command.ModifyOriginalResponseAsync(x => { x.Content = "Please select the account you would like to craft with."; x.Components = builder.Build(); });
             }
 
             user.UpdateAccounts();
             await db.SaveChangesAsync();
+        }
 
+        [ComponentCommand]
+        public static async Task CraftAccountButton(SocketMessageComponent component, DiscordSocketClient _client, Words _words, IServiceProvider _provider, [ComponentData] string data, ApplicationDbContext db) {
+            //await component.UpdateAsync(x => { x.Content = "Working..."; x.Components = null; });
+            var user = await db.DBUsers.FirstAsync(x => x.DiscordId == component.User.Id);
+            if(user is null) return;
+            var dataObjs = data.Split("|");
+            var account = user.EggIncAccounts.FirstOrDefault(x => x.Id == dataObjs[0]);
+            var quality = (TierInput)int.Parse(dataObjs[1]);
+            var quantity = int.Parse(dataObjs[2]);
+            var requestedArtifact = EggIncArtifacts.GetEiAfxData().artifact_families.FirstOrDefault(x => x.id == dataObjs[3]);
 
-            await command.ModifyOriginalResponseAsync(x => {
-                x.Embed = null;
-                x.Content = $"\n{stringBuilder.ToString()}\n";
-            });
+            var contentString = await CraftStringBuilder(account, quantity, quality, requestedArtifact);
+            await component.UpdateAsync(x => { x.Content = contentString; x.Components = null; });
+        }
+
+        private async static Task<string> CraftStringBuilder(EggIncAccount account, int quantity, TierInput quality, ArtifactFamily requestedArtifact) {
+            var stringBuilder = new StringBuilder();
+            var backup = account.Backup;
+            if(backup == null) {
+                return null;
+            }
+
+            backup = new CustomBackup((await ContractsAPI.FirstContact(account.Id)).Backup);
+            stringBuilder.Append($"For **{backup.UserName}** to craft {quantity} T{(int)quality} {requestedArtifact.id}:");
+            stringBuilder.AppendLine();
+
+            var crafter = new Crafter(backup.ArtifactHall);
+            var basket = crafter.GetCraft(quantity, (int)quality, requestedArtifact.id);
+
+            stringBuilder.AppendFormat($"```{"Name",-20}{"Using",-8}{"Need",-8}{"Cost",-8}");
+            stringBuilder.AppendLine();
+            stringBuilder.Append("―――――――――――――――――――――――――――――――――――――――――――");
+            stringBuilder.AppendLine();
+
+            var ingredients = from kvp in basket.GetIngredients()
+                              orderby EggIncArtifacts.GetFamilyShorthand(kvp.Value.Tier.family) ascending, kvp.Value.Tier.tier_number descending
+                              select kvp;
+            foreach(var ingredient in ingredients) {
+                stringBuilder.AppendFormat($"{$"T{ingredient.Value.Tier.tier_number} {EggIncArtifacts.GetFamilyShorthand(ingredient.Value.Tier.family)}",-20}");
+                stringBuilder.AppendFormat($"{ingredient.Value.Use.Format(),-8}");
+                stringBuilder.AppendFormat($"{ingredient.Value.GetNeed(),-8}");
+                stringBuilder.AppendFormat($"{ingredient.Value.Cost.Format(),-8}");
+                stringBuilder.AppendLine();
+            }
+
+            stringBuilder.AppendLine("```");
+            stringBuilder.Append($"Total Cost: **{basket.GetTotalCost().ToString("#,0", new CultureInfo("en-US"))} GE**");
+            stringBuilder.AppendLine();
+            var goldenEggs = backup.GoldenEggsEarned - backup.GoldenEggsSpent;
+            stringBuilder.Append(goldenEggs >= basket.GetTotalCost() ? "You have enough GE!" : "You do not have enough GE!");
+            return stringBuilder.ToString();
         }
 
         [Common.Commands.SlashCommand(Description = "Track your EB since the last time you ran this command")]
