90dece189d5c7dc59e96c52bf28ec4d0191b9655
Joshua Johnson <kendrome@gmail.com>
2023-07-24 10:39:17 -0500

    Embed git info into assembly

--- EGG9000.Bot/Commands/Ping.cs
+++ EGG9000.Bot/Commands/Ping.cs
@@ -6,6 +6,8 @@ using EGG9000.Common.Services;
 
 using System;
 using System.Collections.Generic;
+using System.IO;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -18,22 +20,25 @@ namespace EGG9000.Bot.Commands {
 
         [SlashCommand(Description = "Test to see if bot is alive/check version", AdminOnly = true, AllowFarmHand = true, ParentCommand = "a")]
         public static async Task Ping(FauxCommand command, [SlashParam(Required = false)] bool showInChannel = false) {
-            var result = GitHelpers.ExecuteGitCommand("log -n 1 --format=\"%s%n%H%n%an <%ae>%n%at\"");
 
-            if(result.ExitCode == 0) {
-                var output = result.Output.Trim().Split('\n');
-                var commitMessage = output[0];
-                var commitHash = output[1];
-                var author = output[2];
-                var commitTimestamp = output[3];
-
-                var response = $"___Running commit:___\n**Hash:**\t[{commitHash}](https://github.com/kendrome/EGG9000/commit/{commitHash})" +
-                    $"\n**Author**:\t{author.Split(" ")[0]}\n**Message**:\t{commitMessage}\n**Timestamp:**\t<t:{commitTimestamp}:R>";
-
-                await command.RespondAsync(response, ephemeral: !showInChannel);
-            } else {
-                await command.RespondAsync("Failed to retrieve Git version information.", ephemeral: !showInChannel);
+            string gitVersion = String.Empty;
+    
+            using(Stream stream = Assembly.GetExecutingAssembly()
+                    .GetManifestResourceStream("EGG9000.Bot.version.txt"))
+            using(StreamReader reader = new StreamReader(stream)) {
+                gitVersion = reader.ReadToEnd();
             }
+
+            var output = gitVersion.Trim().Split('\n');
+            var commitMessage = output[0];
+            var commitHash = output[1];
+            var author = output[2];
+            var commitTimestamp = output[3];
+
+            var response = $"___Running commit:___\n**Hash:**\t[{commitHash}](https://github.com/kendrome/EGG9000/commit/{commitHash})" +
+                $"\n**Author**:\t{author.Split(" ")[0]}\n**Message**:\t{commitMessage}\n**Timestamp:**\t<t:{commitTimestamp}:R>";
+
+            await command.RespondAsync(response, ephemeral: !showInChannel);
         }
     }
 }
