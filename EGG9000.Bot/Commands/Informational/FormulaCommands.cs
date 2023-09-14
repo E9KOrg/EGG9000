713cdded6a285b97312226094d29717ba9a7189c
Chandler Dowd <dchandler@xcantech.com>
2023-09-14 18:21:09 -0500

    initial eb function

--- EGG9000.Bot/Commands/Informational/FormulaCommands.cs
+++ EGG9000.Bot/Commands/Informational/FormulaCommands.cs
@@ -147,5 +147,37 @@ namespace EGG9000.Bot.Commands {
 
             sb.AppendLine($"The **LLC** for **{userName}** is `{LLC}` (`{actualLegPercent}%`)\n:tools: Total crafts with legendary possibility: `{craftCount}`\n<:Henerprise:801748924146384906> Henerprises: `{extendedCount}` extended / `{standardCount}` standard / `{shortCount}` short\n<:leggy:1113516502516248636> Legendaries: `{expectedLeg}` expected / `{legCount}` acquired");
         }
+
+        [SlashCommand(Description = "Calculate the EB% based on SE and PE inputs", ParentCommand = "formulae", AllowInDMs = true)]
+        public static async Task Eb(FauxCommand command, [SlashParam(Description = "SE")] string SE, [SlashParam(Description = "PE")] int PE) {
+            await command.RespondAsync("Calculating...");
+
+            double seValue;
+
+            switch(SE.Last()) {
+                case 'q':
+                    seValue = double.Parse(SE.TrimEnd('q')) * 1e15;
+                    break;
+                case 'Q':
+                    seValue = double.Parse(SE.TrimEnd('Q')) * 1e18;
+                    break;
+                case 's':
+                    seValue = double.Parse(SE.TrimEnd('s')) * 1e21;
+                    break;
+                default:
+                    await command.RespondAsync("⚠️ERROR: Invalid SE value: must end with q, Q, or s");
+                    return;
+            }
+
+            if(PE <= 0 || PE > 1000) {
+                await command.RespondAsync("⚠️ERROR: Invalid PE value");
+                return;
+            }
+
+            double result = (seValue * 1.5) * Math.Pow(1.1, PE) * 100;
+            double bonus = Math.Round(Math.Pow((1.05 + 0.01 * 5), PE) * (1.5), 2);
+
+            await command.ModifyOriginalResponseAsync($"{SIPrefix.GetPrefixFromEB(result).RankWithSubRank} (<:Soul_Egg_SE:724341890794913964>`{SE}` and <:Egg_of_Prophecy_PE:669981330477547580>`{PE}`)\nEarnings multiplier: `{result.ToEggString(true, 2)}`\nBonus per soul egg: `{bonus:n}`");
+        }
     }
 }
