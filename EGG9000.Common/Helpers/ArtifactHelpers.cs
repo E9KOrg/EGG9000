92174b30790f36e37cf8b53bcfce1276bc146085
DavidArthurCole <davidarthurcole23@gmail.com>
2023-10-25 00:49:49 -0400

    Oh my god have I not committed any of this yet?

--- EGG9000.Common/Helpers/ArtifactHelpers.cs
+++ EGG9000.Common/Helpers/ArtifactHelpers.cs
@@ -14,24 +14,15 @@ namespace EGG9000.Common.Helpers {
     public class ArtifactHelpers {
 
         public static string GetArtifactFairnessScoreString(List<ArtifactCount> ArtifactHall) {
-            if(ArtifactHall is null || ArtifactHall.Count == 0) return "0 (null artifact hall)";
-            else return GetArtifactFairnessScore(ArtifactHall).ToString("E");
+            return (ArtifactHall is null || ArtifactHall.Count == 0) ? "0 (null artifact hall)" : GetArtifactFairnessScore(ArtifactHall).ToString("E");
         }
 
         public static BigInteger GetArtifactFairnessScore(List<ArtifactCount> ArtifactHall) {
-            if(ArtifactHall is null || ArtifactHall.Count == 0) return 0;
-            else {
-                BigInteger score = 0;
-                foreach(var aCount in ArtifactHall) {
-                    score += (BigInteger)(Math.Pow(GetFairness(aCount.Artifact)[aCount.Artifact.Tier - 1], aCount.Artifact.Rarity + 1) * aCount.Count);
-                }
-                return score;
-            }
+            return (ArtifactHall is null || ArtifactHall.Count == 0) ? 0 : (BigInteger)ArtifactHall.Sum(a => Math.Pow(GetFairness(a.Artifact)[a.Artifact.Tier - 1], a.Artifact.Rarity + 1) * a.Count);
         }
 
         public static long[] GetFairness(EggIncArtifactInstance instance) {
-            if(instance is null || instance.Artifact is null || instance.Tier < 0) return new long[]{0, 0, 0, 0};
-            return instance.Artifact switch {
+            return (instance is null || instance.Artifact is null || instance.Tier < 0) ? new long[] { 0, 0, 0, 0 } : instance.Artifact switch {
                 "Aurelian Brooch" => new long[] { 0, 1186, 13827, 58753 },
                 "Beak of Midas" => new long[] { 0, 6075, 23885, 86083 },
                 "Book of Basan" => new long[] { 0, 114405, 360427, 934701 },
@@ -68,8 +59,7 @@ namespace EGG9000.Common.Helpers {
         }
 
         public static string GetRarityEmoji(EggIncArtifactInstance instance) {
-            if(instance is null) return "";
-            return instance.Rarity switch {
+            return (instance is null) ? "" : instance.Rarity switch {
                 1 => "",
                 2 => "<:Rare:905959988030226453>",
                 3 => "<:Epic:905960149720649748>",
@@ -85,10 +75,10 @@ namespace EGG9000.Common.Helpers {
 
         public static ShipTargetInfo GetTargetInfo(ArtifactSpec.Types.Name aspec) {
             var bInfo = string.Join(" ", Regex.Split(System.Enum.GetName(typeof(ArtifactSpec.Types.Name), aspec), @"(?=\p{Lu})"))
-                .Replace(" Of ", " of ").Replace(" In ", " in ").Replace(" A ", " a ");
-            if(bInfo.StartsWith(" ")) bInfo = bInfo.Substring(1);
+                .Replace(" Of ", " of ").Replace(" In ", " in ").Replace(" A ", " a ").Trim();
             var tier = bInfo.IndexOf("Fragment") > -1 ? (byte)0
-                    : (bInfo.IndexOf(" Stone") > -1 ? (byte)3 : (byte)4);
+                    : (bInfo.IndexOf(" Stone") > -1 || bInfo.IndexOf("Tau Ceti Geode") > -1 || bInfo.IndexOf("Gold Meteorite") > -1 || bInfo.IndexOf("Solar Titanium") > -1 ? (byte)3 
+                    : (byte)4);
             var tempEmoji = GetAfEmoji(new EggIncArtifactInstance() {
                 Artifact = bInfo.Replace(" Fragment", ""),
                 Tier = tier
@@ -100,8 +90,7 @@ namespace EGG9000.Common.Helpers {
         }
 
         public static string GetAfEmoji(EggIncArtifactInstance instance) {
-            if(instance is null || instance.Artifact is null || instance.Tier < 0) return "";
-            return instance.Artifact switch {
+            return (instance is null || instance.Artifact is null || instance.Tier < 0) ? "" : instance.Artifact switch {
                 "Aurelian Brooch" => new string[] { "<:Afx_Aurelian_Brooch_1:801924296213659720>", "<:Afx_Aurelian_Brooch_2:801924317109288981>", "<:Afx_Aurelian_Brooch_3:801924329759571992>", "<:Afx_Aurelian_Brooch_4:801400352338739210>" }[instance.Tier - 1],
                 "Beak of Midas" => new string[] { "<:Afx_Beak_Of_Midas_1:801923763141738527>", "<:Afx_Beak_Of_Midas_2:801923773669441546>", "<:Afx_Beak_Of_Midas_3:801923785963601941>", "<:Afx_Beak_Of_Midas_4:801400369145970688>" }[instance.Tier - 1],
                 "Book of Basan" => new string[] { "<:Afx_Book_Of_Basan_1:801924039102562375>", "<:Afx_Book_Of_Basan_2:801924052922925096>", "<:Afx_Book_Of_Basan_3:801924064180174931>", "<:Afx_Book_Of_Basan_4:801400385230733333>" }[instance.Tier - 1],
@@ -110,7 +99,7 @@ namespace EGG9000.Common.Helpers {
                 "Demeters Necklace" => new string[] { "<:Afx_Demeters_Necklace_1:801791558407159829>", "<:Afx_Demeters_Necklace_2:801791575348609034>", "<:Afx_Demeters_Necklace_3:801791588988092446>", "<:Afx_Demeters_Necklace_4:801400518710788096>" }[instance.Tier - 1],
                 "Dilithium Monocle" => new string[] { "<:Afx_Dilithium_Monocle_1:801786173390979122>", "<:Afx_Dilithium_Monocle_2:801786191748923403>", "<:Afx_Dilithium_Monocle_3:801786210266775562>", "<:Afx_Dilithium_Monocle_4:801402979470278656>" }[instance.Tier - 1],
                 "Dilithium Stone" => new string[] { "<:Afx_Dilithium_Stone_1:807798908897591296>", "<:Afx_Dilithium_Stone_2:807798889901588490>", "<:Afx_Dilithium_Stone_3:807798875153891368>", "<:Afx_Dilithium_Stone_4:801400591363342380>" }[instance.Tier],
-                "Gold Meteorite" => new string[] { "<:Afx_Gold_Meteorite_1:807766396712910898>", "<:Afx_Gold_Meteorite_2:807766382317404171>", "<:Afx_Gold_Meteorite_3:801400672737034240>" }[instance.Tier - 2],
+                "Gold Meteorite" => new string[] { "<:Afx_Gold_Meteorite_1:807766396712910898>", "<:Afx_Gold_Meteorite_2:807766382317404171>", "<:Afx_Gold_Meteorite_3:801400672737034240>" }[instance.Tier - 1],
                 "Gusset" => new string[] { "<:Afx_Ornate_Gusset_1:801790964679180339>", "<:Afx_Ornate_Gusset_2:801790982563561502>", "<:Afx_Ornate_Gusset_3:801790999697293342>", "<:Afx_Ornate_Gusset_4:801400934236160011>" }[instance.Tier - 1],
                 "Interstellar Compass" => new string[] { "<:Afx_Interstellar_Compass_1:801946444600311818>", "<:Afx_Interstellar_Compass_2:801946468734861322>", "<:Afx_Interstellar_Compass_3:801946486627106817>", "<:Afx_Interstellar_Compass_4:801402991776104478>" }[instance.Tier - 1],
                 "Life Stone" => new string[] { "<:Afx_Life_Stone_1:807798970792935426>", "<:Afx_Life_Stone_2:807798955810488321>", "<:Afx_Life_Stone_3:807798940539944990>", "<:Afx_Life_Stone_4:801400782557151252>" }[instance.Tier],
@@ -127,11 +116,11 @@ namespace EGG9000.Common.Helpers {
                 "Quantum Stone" => new string[] { "<:Afx_Quantum_Stone_1:807799549954883624>", "<:Afx_Quantum_Stone_2:807799527498711040>", "<:Afx_Quantum_Stone_3:807799509630582854>", "<:Afx_Quantum_Stone_4:801401076712079381>" }[instance.Tier],
                 "Shell Stone" => new string[] { "<:Afx_Shell_Stone_1:807799916881117214>", "<:Afx_Shell_Stone_2:807799894089007145>", "<:Afx_Shell_Stone_3:807799866142490644>", "<:Afx_Shell_Stone_4:801401100573343776>" }[instance.Tier],
                 "Ship in a Bottle" => new string[] { "<:Afx_Ship_In_A_Bottle_1:801955542238363688>", "<:Afx_Ship_In_A_Bottle_2:801955569858641981>", "<:Afx_Ship_In_A_Bottle_3:801955999123636244>", "<:Afx_Ship_In_A_Bottle_4:801746240785481740>" }[instance.Tier - 1],
-                "Solar Titanium" => new string[] { "<:Afx_Solar_Titanium_1:807261434039894086>", "<:Afx_Solar_Titanium_2:807261357182550067>", "<:Afx_Solar_Titanium_3:801401141605695529>" }[instance.Tier - 2],
+                "Solar Titanium" => new string[] { "<:Afx_Solar_Titanium_1:807261434039894086>", "<:Afx_Solar_Titanium_2:807261357182550067>", "<:Afx_Solar_Titanium_3:801401141605695529>" }[instance.Tier - 1],
                 "Soul Stone" => new string[] { "<:Afx_Soul_Stone_1:807800048662085663>", "<:Afx_Soul_Stone_2:807800028634415144>", "<:Afx_Soul_Stone_3:807799997764075521>", "<:Afx_Soul_Stone_4:801401175487807519>" }[instance.Tier],
                 "Tachyon Deflector" => new string[] { "<:Afx_Tachyon_Deflector_1:801956755655229510>", "<:Afx_Tachyon_Deflector_2:801956779272437800>", "<:Afx_Tachyon_Deflector_3:801956823296770071>", "<:Afx_Tachyon_Deflector_4:801401224292728842>" }[instance.Tier - 1],
                 "Tachyon Stone" => new string[] { "<:Afx_Tachyon_Stone_1:807800396264767528>", "<:Afx_Tachyon_Stone_2:807800375100964864>", "<:Afx_Tachyon_Stone_3:807800354380840980>", "<:Afx_Tachyon_Stone_4:801401251278880788>" }[instance.Tier],
-                "Tau Ceti Geode" => new string[] { "<:Afx_Tau_Ceti_Geode_1:807766532813619240>", "<:Afx_Tau_Ceti_Geode_2:807766597225283617>", "<:Afx_Tau_Ceti_Geode_3:801401276830056488>" }[instance.Tier - 2],
+                "Tau Ceti Geode" => new string[] { "<:Afx_Tau_Ceti_Geode_1:807766532813619240>", "<:Afx_Tau_Ceti_Geode_2:807766597225283617>", "<:Afx_Tau_Ceti_Geode_3:801401276830056488>" }[instance.Tier - 1],
                 "Terra Stone" => new string[] { "<:Afx_Terra_Stone_1:807800715929583626>", "<:Afx_Terra_Stone_2:807800439831658506>", "<:Afx_Terra_Stone_3:807800418549366826>", "<:Afx_Terra_Stone_4:801401301107343390>" }[instance.Tier],
                 "The Chalice" => new string[] { "<:Afx_The_Chalice_1:801923476294205480>", "<:Afx_The_Chalice_2:801923503058059287>", "<:Afx_The_Chalice_3:801923523319693342>", "<:Afx_The_Chalice_4:801401326708850698>" }[instance.Tier - 1],
                 "Titanium Actuator" => new string[] { "<:Afx_Titanium_Actuator_1:801957617253351435>", "<:Afx_Titanium_Actuator_2:801957652052967474>", "<:Afx_Titanium_Actuator_3:801957676808536094>", "<:Afx_Titanium_Actuator_4:801401362763874304>" }[instance.Tier - 1],
@@ -143,31 +132,11 @@ namespace EGG9000.Common.Helpers {
         }
 
         public static uint GetTotalCraftWithLegendaryPossibility(List<ArtifactCount> artifactHall) {
-            if(artifactHall is null || artifactHall.Count == 0) return 0;
-
-            uint totalCrafts = 0;
-
-            foreach(var artifactCount in artifactHall) {
-                var artifactName = artifactCount.Artifact.Artifact;
-
-                if(artifactCount.Artifact.Tier == 4 && artifactName != "Lunar Totem") {
-                    totalCrafts += artifactCount.NumberCrafted;
-                } else if(artifactCount.Artifact.Tier == 3 && artifactName == "Tungsten Ankh") {
-                    totalCrafts += artifactCount.NumberCrafted;
-                }
-            }
-            return totalCrafts;
+            return (artifactHall is null || artifactHall.Count == 0) ? 0 : (uint)artifactHall.Where(a => (a.Artifact.Tier == 4 && a.Artifact.Artifact != "Lunar Totem") || (a.Artifact.Tier == 3 && a.Artifact.Artifact == "Tungsten Ankh")).Sum(c => c.NumberCrafted);
         }
 
         public static int GetLegendaryArtifactCount(List<ArtifactCount> artifactHall) {
-            int legendaryCount = 0;
-
-            foreach (var artifactCount in artifactHall) {
-                if(artifactCount.Artifact.Rarity == 4) {
-                    legendaryCount += artifactCount.Count;
-                }
-            }
-            return legendaryCount;
+            return artifactHall?.Where(a => a.Artifact?.Rarity == 4).Count() ?? 0;
         }
     }
 }
