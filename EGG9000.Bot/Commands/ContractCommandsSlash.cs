07db72e65f1ec0f3fa87c86d65fa62dac51c9420
DavidArthurCole <davidarthurcole23@gmail.com>
2023-11-11 22:47:27 -0500

    'tried to run a command' now checks permissions, changed to embed

--- EGG9000.Bot/Commands/ContractCommandsSlash.cs
+++ EGG9000.Bot/Commands/ContractCommandsSlash.cs
@@ -888,6 +888,10 @@ namespace EGG9000.Bot.Commands {
             return new EmbedBuilder().WithColor(Color.Green).WithDescription(text).WithAuthor(new EmbedAuthorBuilder().WithName("Success").WithIconUrl("https://cdn.discordapp.com/avatars/514257192803893272/47be266c55cab32eacfb33c9affc82dd.webp")).Build();
         }
 
+        public static Embed EmbedWarning(string warningText) {
+            return new EmbedBuilder().WithColor(Color.LightOrange).WithDescription(warningText).WithAuthor(new EmbedAuthorBuilder().WithName("Warning").WithIconUrl("https://cdn.discordapp.com/avatars/514257192803893272/47be266c55cab32eacfb33c9affc82dd.webp")).Build();
+        }
+
         public static Embed EmbedError(string errorText) {
             return new EmbedBuilder().WithColor(Color.Red).WithDescription(errorText).WithAuthor(new EmbedAuthorBuilder().WithName("Error").WithIconUrl("https://cdn.discordapp.com/avatars/514257192803893272/47be266c55cab32eacfb33c9affc82dd.webp")).Build();
         }
