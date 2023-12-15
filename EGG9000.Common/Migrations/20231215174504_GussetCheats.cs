6b4da7d2c0c0d381b00d56f7e068580dd70282f3
DavidArthurCole <davidarthurcole23@gmail.com>
2023-12-15 13:50:03 -0500

    Gusset cheating tracking

---
+++ EGG9000.Common/Migrations/20231215174504_GussetCheats.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EGG9000.Common.Migrations
+{
+    public partial class GussetCheats : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "GussetCheatDetected",
+                table: "UserCoopXrefs",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "GussetCheatDetected",
+                table: "UserCoopXrefs");
+        }
+    }
+}
