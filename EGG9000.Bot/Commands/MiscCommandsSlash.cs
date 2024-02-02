dcbc9acb56380b2430b0e74592c11790fec7d6cf
DavidArthurCole <davidarthurcole23@gmail.com>
2024-02-01 19:52:21 -0500

    Edit wording for RSC message

--- EGG9000.Bot/Commands/MiscCommandsSlash.cs
+++ EGG9000.Bot/Commands/MiscCommandsSlash.cs
@@ -258,7 +258,7 @@ Last Backup <t:{backup.LastBackupTime}:R>
         [ComponentCommand]
         public static async Task WhatIsRSC(SocketMessageComponent component) {
             var rscText = "The Contract Eggspert role is awarded to the top 10 highest scoring players of each scored contract, as well as the top-performers in Grades C, B, and A. The role will be removed after 7 days, and serves only to recognize eggceptional performance.\n\n" +
-                "Score is determined by comparing a player's `Total Eggs Delivered` to the 100 closest players in EB (50 above, 50 below). The number (score) next to a name denotes how many times greater than average the user's total was.\n\n" +
+                "Score is determined by comparing a player's `Total Eggs Delivered` to the 100 closest players in EB (50 above, 50 below). The number (score) next to a name denotes how many times greater than average the user's total was. I.e., a score of 1 is average, a score of 2 is double the average, etc.\n\n" +
                 "Contracts are scored manually by Palace staff once all Palace coops have finished, and the contract has expired. You can read more about scoring, and Running Score, in this announcement: https://discord.com/channels/656455567858073601/698270110279925770/939264092445745163";
             var rscEmbed = new EmbedBuilder().WithColor(Color.LighterGrey).WithDescription(rscText).WithAuthor(new EmbedAuthorBuilder().WithName("What is this?").WithIconUrl("https://cdn.discordapp.com/avatars/514257192803893272/47be266c55cab32eacfb33c9affc82dd.webp")).Build();
 
