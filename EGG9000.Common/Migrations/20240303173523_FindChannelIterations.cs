9d6d392b8ca689ba1fbf1a6b119d40be784e1d87
DavidArthurCole <davidarthurcole23@gmail.com>
2024-03-03 12:35:54 -0500

    Add failsafe for manually-deleted channels

---
+++ EGG9000.Common/Migrations/20240303173523_FindChannelIterations.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EGG9000.Common.Migrations
+{
+    public partial class FindChannelIterations : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<long>(
+                name: "FindChannelErrors",
+                table: "Coops",
+                type: "bigint",
+                nullable: false,
+                defaultValue: 0L);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "FindChannelErrors",
+                table: "Coops");
+        }
+    }
+}
