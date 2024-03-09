5da476a75346f4969f5092d3c573301bc0d12751
DavidArthurCole <davidarthurcole23@gmail.com>
2024-03-08 20:51:02 -0500

    So much, see PR

--- EGG9000.Common/Helpers/MissionHelpers.cs
+++ EGG9000.Common/Helpers/MissionHelpers.cs
@@ -37,6 +37,12 @@ namespace EGG9000.Common.Helpers {
                 )
             );
 
+        private static readonly Dictionary<Spaceship, List<int>> LevelRequirements
+            = Root.Get().missionParameters.ToDictionary(
+                mp => mp.shipEnum,
+                mp => mp.levelMissionRequirements
+            );
+
         public static int GetNominalCapacity(this CustomBackup backup, SpaceMission mission) {
             if(mission is null || !NominalShipCapacities.ContainsKey(mission.Ship)) return 0;
             var nominalCaps = NominalShipCapacities[mission.Ship][mission.Duration];
@@ -50,7 +56,7 @@ namespace EGG9000.Common.Helpers {
             if(erScalar < 0 || ship < Spaceship.MilleniumChicken) erScalar = 0;
             var shipTimes = ShipBaseTimesMinutes[ship];
             return shipTimes.ContainsKey(duration) ?
-                MinutesToString(backup, (int)(ShipBaseTimesMinutes[ship][duration] * ((double)number / 3) * (double)(1 - (.01 * erScalar)))) //Div by 3 for 3 ship slots
+                MinutesToString((int)(ShipBaseTimesMinutes[ship][duration] * ((double)number / 3) * (double)(1 - (.01 * erScalar)))) //Div by 3 for 3 ship slots
                 : "";
         }
 
@@ -64,69 +70,28 @@ namespace EGG9000.Common.Helpers {
                 : 0;
         }
 
-        public static string MinutesToString(this CustomBackup backup, int minutes) {
-            if(minutes < 60) return minutes + "m";
-            if(minutes < 1440) { // Less than a day
-                var hours = minutes / 60;
-                var remainingMinutes = minutes % 60;
-                return remainingMinutes == 0 ? hours + "h" : hours + "h" + remainingMinutes + "m";
-            }
-            if(minutes < 525600) { // Less than a year (assuming 365 days in a year)
-                var days = minutes / 1440;
-                var remainingMinutes = minutes % 1440;
-                if(remainingMinutes == 0) return days + "d";
-                var hours = remainingMinutes / 60;
-                var remainingMinutesInHourSc = remainingMinutes % 60;
-                return days + "d" + hours + "h" + remainingMinutesInHourSc + "m";
-            }
-            var years = minutes / 525600;
-            var remainingMinutesInYear = minutes % 525600;
-            if(remainingMinutesInYear == 0) return years + "y";
-            var daysInYear = remainingMinutesInYear / 1440;
-            var remainingMinutesInDay = remainingMinutesInYear % 1440;
-            var hoursInDay = remainingMinutesInDay / 60;
-            var remainingMinutesInHour = remainingMinutesInDay % 60;
-            var result = years + "y";
-            if(daysInYear > 0)
-                result += daysInYear + "d";
-            if(hoursInDay > 0)
-                result += hoursInDay + "h";
-            if(remainingMinutesInHour > 0)
-                result += remainingMinutesInHour + "m";
-            return result;
-        }
+        public static string MinutesToString(int minutes) {
+            var timeSpan = TimeSpan.FromMinutes(minutes);
 
-        public static List<MissionInfo> GetLaunchedMissions(ArtifactsDB artifactsDB) {
-            return (artifactsDB.MissionArchive.ToList() ?? new List<MissionInfo>())
-                .Concat(artifactsDB.MissionInfos.ToList() ?? new List<MissionInfo>())
-                .Where(m => m.Status != Status.Exploring)
-                .OrderByDescending(m => m.StartTimeDerived).ToList();
-        }
+            if(timeSpan.TotalMinutes < 60) return $"{timeSpan.Minutes}m";
 
-        public static Dictionary<Spaceship, uint> GetShipLevels(ArtifactsDB artifactsDB) {
-            var newDic = new Dictionary<Spaceship, uint>();
-            GetLaunchedMissions(artifactsDB).GroupBy(m => m.Ship).Select(group => group.OrderByDescending(ship => ship.Level).First()).ToList().ForEach(l => {
-                newDic.Add(l.Ship, l.Level);
-            });
-            return newDic;
+            if(timeSpan.TotalMinutes < 1440)
+                return $"{timeSpan.Hours}h{(timeSpan.Minutes > 0 ? $"{timeSpan.Minutes}m" : "")}";
+
+            if(timeSpan.TotalMinutes < 525600)
+                return $"{timeSpan.Days}d{timeSpan.Hours}h{(timeSpan.Minutes > 0 ? $"{timeSpan.Minutes}m" : "")}";
+
+            return $"{(int)(timeSpan.TotalDays / 365)}y{timeSpan.Days % 365}d{timeSpan.Hours}h{(timeSpan.Minutes > 0 ? $"{timeSpan.Minutes}m" : "")}";
         }
 
         public static int GetShipLevel(this CustomBackup backup, Spaceship ship) {
-            if(backup.ShipsSent == null)
-                return 0;
+            if(backup.ShipsSent == null) return 0;
 
             //If they don't have the ship unlocked
             if(!backup.ShipsSent.Any(x => x.ship == ship)) return 0;
 
-            var points = backup.ShipsSent.FirstOrDefault(x => x.ship == ship && x.type == DurationType.Short).count +
-                backup.ShipsSent.FirstOrDefault(x => x.ship == ship && x.type == DurationType.Long).count * 1.4 +
-                backup.ShipsSent.FirstOrDefault(x => x.ship == ship && x.type == DurationType.Epic).count * 1.8;
-
-            var levelMissionRequirements = Root.Get().missionParameters.First(x => ship.ToString().Equals(x.ship.Replace("_", ""), StringComparison.CurrentCultureIgnoreCase)).levelMissionRequirements;
-
-            for(var i = levelMissionRequirements.Count; i > 0; i--) {
-                var sum = levelMissionRequirements.Take(i).Sum();
-                if(points >= sum) {
+            for(var i = LevelRequirements[ship].Count; i > 0; i--) {
+                if(backup.ShipsSent.Where(x => x.ship == ship).ToList().Sum(s => s.count * ((int)s.type < 3 ? (1 + ((int)s.type * .4)) : 0)) >= LevelRequirements[ship].Take(i).Sum()) {
                     return i;
                 }
             }
@@ -141,7 +106,7 @@ namespace EGG9000.Common.Helpers {
                 Spaceship.ChickenHeavy => "Chicken Heavy",
                 Spaceship.Bcr => "BCR",
                 Spaceship.MilleniumChicken => "Quintillion Chicken",
-                Spaceship.CorellihenCorvette => "Cornish-Hen Corbette",
+                Spaceship.CorellihenCorvette => "Cornish-Hen Corvette",
                 Spaceship.Galeggtica => "Galleggtica",
                 Spaceship.Chickfiant => "Defihent",
                 Spaceship.Voyegger => "Voyegger",
@@ -152,98 +117,72 @@ namespace EGG9000.Common.Helpers {
         }
 
         public static Dictionary<DurationType, int> LaunchShipsToNext(this CustomBackup backup, Spaceship ship) {
-            if(backup.ShipsSent is null) return new();
+            if(backup.ShipsSent is null) return [];
 
-            var points = (!backup.ShipsSent.Any(x => x.ship == ship)) ? 0 : backup.ShipsSent.FirstOrDefault(x => x.ship == ship && x.type == DurationType.Short).count +
-            backup.ShipsSent.FirstOrDefault(x => x.ship == ship && x.type == DurationType.Long).count * 1.4 +
-            backup.ShipsSent.FirstOrDefault(x => x.ship == ship && x.type == DurationType.Epic).count * 1.8;
-
-            var levelMissionRequirements = Root.Get().missionParameters.First(x => ship.ToString().Equals(x.ship.Replace("_", ""), StringComparison.CurrentCultureIgnoreCase)).levelMissionRequirements;
+            var points = backup.ShipsSent.Where(x => x.ship == ship).ToList().Sum(s => s.count * ((int)s.type < 3 ? (1 + ((int)s.type * .4)) : 0));
+            var levelMissionRequirements = LevelRequirements[ship];
 
             for(var i = levelMissionRequirements.Count; i > 0; i--) {
                 if(points >= levelMissionRequirements.Take(i).Sum()) {
                     return ShipsFromPoints(levelMissionRequirements.Take(i + 1).Sum() - points);
                 }
             }
-            if(levelMissionRequirements.Count == 0) return new();
-            if(points == 0) return ShipsFromPoints(levelMissionRequirements[0]);
+            if(levelMissionRequirements.Count == 0) return [];
+            if(points < levelMissionRequirements[0]) return ShipsFromPoints(levelMissionRequirements[0] - points);
             else return ShipsFromPoints(0);
         }
 
         public static Dictionary<DurationType, int> LaunchShipsToMax(this CustomBackup backup, Spaceship ship) {
-            if(backup.ShipsSent is null) return new();
-
-            var points = (!backup.ShipsSent.Any(x => x.ship == ship)) ? 0 : backup.ShipsSent.FirstOrDefault(x => x.ship == ship && x.type == DurationType.Short).count +
-                backup.ShipsSent.FirstOrDefault(x => x.ship == ship && x.type == DurationType.Long).count * 1.4 +
-                backup.ShipsSent.FirstOrDefault(x => x.ship == ship && x.type == DurationType.Epic).count * 1.8;
-
-            var levelMissionRequirements = Root.Get().missionParameters.First(x => ship.ToString().Equals(x.ship.Replace("_", ""), StringComparison.CurrentCultureIgnoreCase)).levelMissionRequirements;
-            return ShipsFromPoints(levelMissionRequirements.Sum() - points);
+            if(backup.ShipsSent is null) return [];
+            var points = backup.ShipsSent.Where(x => x.ship == ship).ToList().Sum(s => s.count * ((int)s.type < 3 ? (1 + ((int)s.type * .4)) : 0));
+            return ShipsFromPoints(LevelRequirements[ship].Sum() - points);
         }
 
         public static Dictionary<DurationType, int> ShipsFromPoints(double neededPoints) {
-            Dictionary<DurationType, int> newDic = new();
-            if(neededPoints == 0) return newDic;
-
-            newDic.Add(DurationType.Short, (int)Math.Ceiling(neededPoints / 1));
-            newDic.Add(DurationType.Long, (int)Math.Ceiling(neededPoints / 1.4));
-            newDic.Add(DurationType.Epic, (int)Math.Ceiling(neededPoints / 1.8));
-
-            return newDic;
+            return Enum.GetValues<DurationType>().ToDictionary(
+               d => d,
+               d => (int)Math.Ceiling(neededPoints / (1 + ((int)d * 0.4)))
+            );
         }
 
         public static double GetShipProgressNextLevel(this CustomBackup backup, Spaceship ship) {
-            if(backup.ShipsSent == null)
-                return 0;
-
-            //No stars, always maxed
+            if(backup.ShipsSent == null) return 0;
             if(ship == Spaceship.ChickenOne) return 1;
 
-            var points = (!backup.ShipsSent.Any(x => x.ship == ship)) ? 0 : backup.ShipsSent.FirstOrDefault(x => x.ship == ship && x.type == DurationType.Short).count  +
-                backup.ShipsSent.FirstOrDefault(x => x.ship == ship && x.type == DurationType.Long).count * 1.4 +
-                backup.ShipsSent.FirstOrDefault(x => x.ship == ship && x.type == DurationType.Epic).count * 1.8 ;
+            var points = backup.ShipsSent.Where(x => x.ship == ship).ToList().Sum(s => s.count * ((int)s.type < 3 ? (1 + ((int)s.type * .4)) : 0));
 
-            var levelMissionRequirements = Root.Get().missionParameters.First(x => ship.ToString().Equals(x.ship.Replace("_", ""), StringComparison.CurrentCultureIgnoreCase)).levelMissionRequirements;
+            var levelMissionRequirements = Root.Get().missionParameters.First(x => x.shipEnum == ship).levelMissionRequirements;
 
             for(var i = levelMissionRequirements.Count; i > 0; i--) {
                 var sum = levelMissionRequirements.Take(i).Sum();
                 if(points >= sum) {
                     if(levelMissionRequirements.Count > i) {
-                        double numLaunchedCurrentLevel = points - sum;
+                        var numLaunchedCurrentLevel = points - sum;
                         double numForNextLevel = levelMissionRequirements[i];
                         return numLaunchedCurrentLevel / numForNextLevel;
                     } else {
                         return 1;
                     }
-                    
                 }
             }
             return 0;
         }
 
         public static double GetShipProgressTotal(this CustomBackup backup, Spaceship ship) {
-            if(backup.ShipsSent == null)
-                return 0;
+            if(backup.ShipsSent == null) return 0;
+            var points = backup.ShipsSent.Where(x => x.ship == ship).ToList().Sum(s => s.count * ((int)s.type < 3 ? (1 + ((int)s.type * .4)) : 0));
+            double sum = LevelRequirements[ship].Sum();
 
-            var points = (!backup.ShipsSent.Any(x => x.ship == ship)) ? 0 : backup.ShipsSent.FirstOrDefault(x => x.ship == ship && x.type == DurationType.Short).count +
-                backup.ShipsSent.FirstOrDefault(x => x.ship == ship && x.type == DurationType.Long).count * 1.4 +
-                backup.ShipsSent.FirstOrDefault(x => x.ship == ship && x.type == DurationType.Epic).count * 1.8;
-
-            var levelMissionRequirements = Root.Get().missionParameters.First(x => ship.ToString().Equals(x.ship.Replace("_", ""), StringComparison.CurrentCultureIgnoreCase)).levelMissionRequirements;
-
-            var sum = levelMissionRequirements.Sum();
+            if(ship == Spaceship.Atreggies) {
+                Console.WriteLine("Points: " + points);
+                Console.WriteLine("Sum: " + sum);
+            }
 
             return (points == 0 ? 0 : (points / sum > 1 ? 1 : points / sum));
         }
 
-        public static bool HasMaxedShips(CustomBackup backup) {
-            return MaxShipLevels.All(x => {
-                var currentStars = backup.GetShipLevel(x.Key);
-                if(currentStars == x.Value) {
-                    return true;
-                }
-                return false;
-            });
+        public static bool HasMaxedShips(this CustomBackup backup) {
+            return MaxShipLevels.All(x => backup.GetShipLevel(x.Key) == x.Value);
         }
     }
 }
