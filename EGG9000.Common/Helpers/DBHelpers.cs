8eb3b6c0d9a151e6024043253b494b85382dbc5c
David Cole <40234707+DavidArthurCole@users.noreply.github.com>
2024-04-10 11:22:38 -0400

    Semaphores for header channels

---
+++ EGG9000.Common/Helpers/DBHelpers.cs
@@ -0,0 +1,22 @@
+﻿using EGG9000.Common.Database;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace EGG9000.Common.Helpers {
+    public static class DBHelpers {
+
+        public static async Task<(bool, int)> SaveChangesAsyncRetry(this ApplicationDbContext db, int retryCount = 1, CancellationToken cancellationToken = default) {
+            var currentRetry = 0;
+            while(true) {
+                try {
+                    return (true, await db.SaveChangesAsync(cancellationToken));
+                } catch(Exception) {
+                    if(currentRetry >= retryCount) return (false, currentRetry);
+                    currentRetry++;
+                }
+            }
+        }
+
+    }
+}
