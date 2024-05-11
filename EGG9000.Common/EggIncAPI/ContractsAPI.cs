66d253b1459fe3efecbde1e0ad11ea04d5975bfa
Kendrome <kendrome@gmail.com>
2024-05-10 20:06:39 -0500

    Update to ClientVersion 66

--- EGG9000.Common/EggIncAPI/ContractsAPI.cs
+++ EGG9000.Common/EggIncAPI/ContractsAPI.cs
@@ -32,7 +32,7 @@ namespace EGG9000.Bot.EggIncAPI {
         //static string BaseAddressOld = "http://afx-2-dot-auxbrainhome.appspot.com/";
         public const string BaseAddressNew = "https://www.auxbrain.com/";
         public const string UserId = "EI5223299518300160";
-        public static uint ClientVersion { get; set; } = 65;
+        public static uint ClientVersion { get; set; } = 66;
 
         public static BasicRequestInfo GetInfo(string UserId, bool noUserID = false) {
             var info = new BasicRequestInfo {
