3056ebb5cc0f7ccfe12d660588f3537e1ee02d1c
Kendrome <kendrome@gmail.com>
2026-02-09 22:30:27 -0600

    Docker work

--- BotTesting/Program.cs
+++ BotTesting/Program.cs
@@ -13,6 +13,8 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 
+using Newtonsoft.Json;
+
 using NLog;
 using NLog.Web;
 
@@ -65,11 +67,14 @@ using NLog.Web;
 
 
 var bugsnag = new Bugsnag.Client(new Bugsnag.Configuration("d5141d01f0e9f20506c3dcaa9110fe01"));
+//Bugsnag.AspNet.Core.HttpContextExtensions.ToRequest
+//new Bugsnag.Configuration { }
 
-try {
-    throw new System.NotImplementedException();
-} catch(System.Exception ex) {
-    bugsnag.Notify(ex);
-}
+//try {
+//    throw new System.NotImplementedException();
+//} catch(System.Exception ex) {
+//    bugsnag.Notify(ex);
+//}
 
-await Task.Delay(1000);
+//await Task.Delay(1000);
+Console.WriteLine(JsonConvert.SerializeObject(bugsnag.Configuration));
