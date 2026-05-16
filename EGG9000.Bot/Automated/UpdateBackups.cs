eb3fdde18b1f89fb61c15417b756d73e8eebb5da
DavidArthurCole <davidarthurcole23@gmail.com>
2026-05-15 22:46:07 -0400

    Re-add ultra logging

--- EGG9000.Bot/Automated/UpdateBackups.cs
+++ EGG9000.Bot/Automated/UpdateBackups.cs
@@ -76,7 +76,7 @@ namespace EGG9000.Bot.Automated {
 
                     var guild = _client.Guilds.FirstOrDefault(x => x.Id == user.GuildId);
                     var dbGuild = guilds.FirstOrDefault(x => x.Id == user.GuildId);
-                    //await account.UpdateSubscriptionFromCustomBackup(_client, guild, dbGuild, user);
+                    await account.UpdateSubscriptionFromCustomBackup(_client.Gateway, guild, dbGuild, user);
                     update = true;
 
                     // TODO: Track current game version and notify if newer version is available
