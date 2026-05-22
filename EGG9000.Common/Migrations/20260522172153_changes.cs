5c6eec74611dd6ce0a838b1d4629446591862de2
Kendrome <kendrome@gmail.com>
2026-05-22 18:45:00 -0500

    - Add Timings logging to CreateCoopThreads - Add Index for guilds

---
+++ EGG9000.Common/Migrations/20260522172153_changes.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EGG9000.Common.Migrations
+{
+    /// <inheritdoc />
+    public partial class changes : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateIndex(
+                name: "IX_Guilds_DiscordSeverId",
+                table: "Guilds",
+                column: "DiscordSeverId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Guilds_DiscordSeverId",
+                table: "Guilds");
+        }
+    }
+}
