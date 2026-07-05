7cab21c2e7d73dcf13c0ded72ffa6c547ca8ee1b
Harsharoni <me@harshzalawadia.com>
2026-07-05 01:00:29 +0100

    fix: build errors

--- EGG9000.Bot/Automated/Coops/ThreadsCoopStatusUpdater.cs
+++ EGG9000.Bot/Automated/Coops/ThreadsCoopStatusUpdater.cs
@@ -452,15 +452,16 @@ namespace EGG9000.Bot.Automated.Coops {
 
 
                 var waitingOn = usersWithStatus.Where(x => !x.Status?.Finalized ?? false);
+                var isFinished = coop.Finished || status.Finished();
                 if(!coop.FinalizedFinishedOrFailed()) {
                     await CheckHighestEBJoined(coop, usersWithStatus, coopDetails, coopThread, _db, usersNotJoined);
 
-                    if(!status.Finished && !coop.ProjectedToFinish && coopDetails.PercentProjectedForJoined >= 100 && coop.CoopEnds > DateTimeOffset.UtcNow) {
+                    if(!isFinished && !coop.ProjectedToFinish && coopDetails.PercentProjectedForJoined >= 100 && coop.CoopEnds > DateTimeOffset.UtcNow) {
                         coop.ProjectedToFinish = true;
                         _queue.EnqueueLow(() => coopThread.SendMessageAsync($"Coop {coop.Name} is now projected to finish!"));
                     }
 
-                    if(!status.Finished && status.SecondsRemaining > 1 && coop.ProjectedToFinish && coopDetails.PercentProjectedForJoined < 100 && coop.CoopEnds > DateTimeOffset.UtcNow) {
+                    if(!isFinished && status.SecondsRemaining > 1 && coop.ProjectedToFinish && coopDetails.PercentProjectedForJoined < 100 && coop.CoopEnds > DateTimeOffset.UtcNow) {
                         coop.ProjectedToFinish = false;
                         _queue.EnqueueLow(() => coopThread.SendMessageAsync($"Coop {coop.Name} is **no longer** projected to finish."));
                     }
