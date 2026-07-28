eef3450ea6569311848ae0bf6dafc0a9a07c2912
DavidArthurCole <davidarthurcole23@gmail.com>
2026-07-27 21:14:06 -0400

    chore: cleanup of test rendering

--- EGG9000.Bot/Automated/Coops/ThreadsCoopStatusUpdater.MessageSizing.cs
+++ EGG9000.Bot/Automated/Coops/ThreadsCoopStatusUpdater.MessageSizing.cs
@@ -1,5 +1,8 @@
 using Discord;
 using System;
+using System.Collections.Generic;
+using System.Linq;
+using static EGG9000.Common.Helpers.FixedWidthTable;
 
 namespace EGG9000.Bot.Automated.Coops {
     public partial class ThreadsCoopStatusUpdater {
@@ -28,12 +31,39 @@ namespace EGG9000.Bot.Automated.Coops {
             return (int)Math.Ceiling(totalChars / (double)ComponentsV2Safe.TextDisplayMax);
         }
 
-        internal static string BuildWorstCaseFillerText(int maxUsers) {
-            var unjoinedChars = UnjoinedListOverheadCharsWorstCase + maxUsers * UnjoinedMentionCharsWorstCase;
-            var extrasChars = unjoinedChars + GiftTableCharsWorstCase + CommandLinksCharsWorstCase + GradeWarningCharsWorstCase;
-            return new string('X', extrasChars);
+        internal static string BuildWorstCaseFillerText(int maxUsers) =>
+            BuildWorstCaseUnjoinedList(maxUsers) + BuildWorstCaseGiftTable() + BuildWorstCaseCommandLinks() + BuildWorstCaseGradeWarning();
+
+        internal static string BuildWorstCaseHeaderText() =>
+            "# TESTSEED-WorstCaseCoopName99\nGrade AAA - simulated worst-case coop status embed\n-# Updated just now - simulated worst-case content, not a real coop";
+
+        private static string BuildWorstCaseUnjoinedList(int maxUsers) {
+            var mentions = Enumerable.Range(0, maxUsers).Select(i =>
+                Truncate($"<@{100000000000000000L + i}> (WorstCaseFarmerName{i}) (Wrong GradeAAA) (Missing from server)", UnjoinedMentionCharsWorstCase));
+            return $"Coop **TESTSEED-WorstCaseCoopName99** is ready for the following to join: {string.Join(", ", mentions)}\n";
         }
 
-        internal static string BuildWorstCaseHeaderText() => new string('X', EmbedHeaderCharsWorstCase);
+        private static string BuildWorstCaseGiftTable() {
+            List<List<FixedWidthCell>> table = [
+                [new(""), new("🐔", CellAlignment.Center), new("🏠", CellAlignment.Center), new("🚚", CellAlignment.Center)],
+                .. Enumerable.Range(0, 10).Select(i => new List<FixedWidthCell> {
+                    new(Truncate($"WorstCasePlyr{i}", 11)),
+                    new("999.99q", CellAlignment.Right),
+                    new("99%", CellAlignment.Right),
+                    new("99%", CellAlignment.Right),
+                }),
+            ];
+            return $"\nFarms that would benefit from gifting chickens: \n```{GetTable(table)}```\n\n";
+        }
+
+        private static string BuildWorstCaseCommandLinks() =>
+            "__Co-op Commands (click to use):__\n" +
+            "\n</callstaff:999999999999999999> Use this command if you joined a co-op for the wrong contract, or have other questions or concerns" +
+            "\n</coopsettings:999999999999999999> Receive DM pings for various events in the co-op" +
+            "\n</fixfullcooperror:999999999999999999> If you get the error co-op is full, try running this command to free up the space.";
+
+        private static string BuildWorstCaseGradeWarning() =>
+            " Warning! Looks like this co-op is the wrong grade and is actually GradeAAA" +
+            "\n\nWaiting on the following users to check-in: <@100000000000000000>, <@100000000000000001>, <@100000000000000002>";
     }
 }
