028e14450bf10f231cea3f006477d2e8c1a19de0
DavidArthurCole <davidarthurcole23@gmail.com>
2026-08-24 11:56:46 -0400

    chore: primary constructors

--- EGG9000.Bot/Interactions/UserParams.cs
+++ EGG9000.Bot/Interactions/UserParams.cs
@@ -15,37 +15,29 @@ namespace EGG9000.Bot.Interactions {
     // has left the server. Use UserSlots (SocketUser) for anything that should still work on a
     // departed user (ban, kick, merit); use GuildUserSlots only when the command needs guild-member
     // state, e.g. role assignment.
-    public class UserSlots {
-        public SocketUser[] Users { get; }
-
-        [ComplexParameterCtor]
-        public UserSlots(
-            [Summary("user1")] SocketUser user1,
-            [Summary("user2")] SocketUser user2 = null,
-            [Summary("user3")] SocketUser user3 = null,
-            [Summary("user4")] SocketUser user4 = null,
-            [Summary("user5")] SocketUser user5 = null,
-            [Summary("user6")] SocketUser user6 = null,
-            [Summary("user7")] SocketUser user7 = null,
-            [Summary("user8")] SocketUser user8 = null) {
-            Users = new[] { user1, user2, user3, user4, user5, user6, user7, user8 }.Where(u => u is not null).ToArray();
-        }
+    [method: ComplexParameterCtor]
+    public class UserSlots(
+        [Summary("user1")] SocketUser user1,
+        [Summary("user2")] SocketUser user2 = null,
+        [Summary("user3")] SocketUser user3 = null,
+        [Summary("user4")] SocketUser user4 = null,
+        [Summary("user5")] SocketUser user5 = null,
+        [Summary("user6")] SocketUser user6 = null,
+        [Summary("user7")] SocketUser user7 = null,
+        [Summary("user8")] SocketUser user8 = null) {
+        public SocketUser[] Users { get; } = new[] { user1, user2, user3, user4, user5, user6, user7, user8 }.Where(u => u is not null).ToArray();
     }
 
-    public class GuildUserSlots {
-        public SocketGuildUser[] Users { get; }
-
-        [ComplexParameterCtor]
-        public GuildUserSlots(
-            [Summary("user1")] SocketGuildUser user1,
-            [Summary("user2")] SocketGuildUser user2 = null,
-            [Summary("user3")] SocketGuildUser user3 = null,
-            [Summary("user4")] SocketGuildUser user4 = null,
-            [Summary("user5")] SocketGuildUser user5 = null,
-            [Summary("user6")] SocketGuildUser user6 = null,
-            [Summary("user7")] SocketGuildUser user7 = null,
-            [Summary("user8")] SocketGuildUser user8 = null) {
-            Users = new[] { user1, user2, user3, user4, user5, user6, user7, user8 }.Where(u => u is not null).ToArray();
-        }
+    [method: ComplexParameterCtor]
+    public class GuildUserSlots(
+        [Summary("user1")] SocketGuildUser user1,
+        [Summary("user2")] SocketGuildUser user2 = null,
+        [Summary("user3")] SocketGuildUser user3 = null,
+        [Summary("user4")] SocketGuildUser user4 = null,
+        [Summary("user5")] SocketGuildUser user5 = null,
+        [Summary("user6")] SocketGuildUser user6 = null,
+        [Summary("user7")] SocketGuildUser user7 = null,
+        [Summary("user8")] SocketGuildUser user8 = null) {
+        public SocketGuildUser[] Users { get; } = new[] { user1, user2, user3, user4, user5, user6, user7, user8 }.Where(u => u is not null).ToArray();
     }
 }
